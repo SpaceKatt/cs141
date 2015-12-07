79260187d64ace7a7947bb78b4b18470ee59c569
Thomas Kercheval <spacekattpoispin@gmail.com>
2015-12-07 15:57:29 -0800

    Progress on Lab19.

--- src/cs141/lab19/Q3.java
+++ src/cs141/lab19/Q3.java
@@ -20,11 +20,13 @@ package cs141.lab19;
  *
  * @author Thomas
  */
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JComboBox;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 
 public class Q3 {
@@ -39,20 +41,87 @@ public class Q3 {
 }
 
 class ColorFrame extends JFrame {
-    
-    JMenuBar menu;
+    private int[] color;
+    private JMenuBar menu;
     
     public ColorFrame() {
+        color = new int[]{0,0,0};
         menu = new JMenuBar();
-        setJMenuBar(menu);
+        
+        createMenu();
+        
+        setSize(800, 800);
     }
     
     private void createMenu() {
-        JMenu fileMenu = new JMenu("File");
-        JMenu color = new JMenu("Color");
+        setJMenuBar(menu);
         
-        menu.add(fileMenu);
-        menu.add(color);
+        menu.add(createFileMenu());
+        menu.add(createColorMenu());
+    }
+    
+    class ExitItemListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.exit(0);
+        }
+    }
+
+    private JMenu createFileMenu() {
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem exitItem = new JMenuItem("Exit");
+        ActionListener listener = new ExitItemListener();
+        exitItem.addActionListener(listener);
+        fileMenu.add(exitItem);
+        return fileMenu;
+    }
+
+    private JMenu createColorMenu() {
+        JMenu col = new JMenu("Color");
+        col.add(createColorItem("Red"));
+        col.add(createColorItem("Green"));
+        col.add(createColorItem("Blue"));
+        return col;
+    }
+
+    private JMenuItem createColorItem(final String c) {
+        class ColorItemListener implements ActionListener {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                switch (c) {
+                    case "Red":
+                        color[0] = 255;
+                        color[1] = 0;
+                        color[2] = 0;
+                        break;
+                    case "Green":
+                        color[0] = 0;
+                        color[1] = 255;
+                        color[2] = 0;
+                        break;
+                    default:
+                        color[0] = 0;
+                        color[1] = 0;
+                        color[2] = 255;
+                        break;
+                }
+                setColor();
+            }
+        }
+        
+        JMenuItem item = new JMenuItem(c);
+        ActionListener listener = new ColorItemListener();
+        item.addActionListener(listener);
+        return item;
+    }
+    
+    private void setColor() {
+        int red = color[0];
+        int blue = color[2];
+        int green = color[1];
+        
+        Color customColor = new Color(red, green, blue);
+        getContentPane().setBackground(customColor);
     }
 }
 
